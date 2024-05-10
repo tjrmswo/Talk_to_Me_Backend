b78e67600513b518012bb2c24e1c4fc4aa2989f6
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-05-10 18:46:31 +0900

    feat: CRUD 간단 구현

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,22 +1,22 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity({ name: 'user' })
 export class User {
   @PrimaryGeneratedColumn()
   email: string;
 
-  @Column({ type: 'varchar' })
+  @Column({ type: 'varchar', length: 45 })
   ID: string;
 
-  @Column({ type: 'varchar' })
+  @Column({ type: 'varchar', length: 40 })
   nickname: string;
 
-  @Column({ type: 'varchar' })
+  @Column({ type: 'varchar', length: 45 })
   password: string;
 
-  @Column({ type: 'varchar' })
+  @Column({ type: 'varchar', length: 45 })
   passwordCheck: string;
 
-  @Column({ type: 'varchar' })
+  @Column({ type: 'varchar', length: 255 })
   interests: string;
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
-
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
   findAll() {
-    return `This action returns all user`;
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(email: string) {
+    return this.userRepository.findOneBy({ email: email });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  create(createUserDto: CreateUserDto) {
+    return this.userRepository.save(createUserDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(email: string) {
+    return this.userRepository.delete(email);
+  }
+
+  update(email: string, updateUserDto: UpdateUserDto) {
+    return this.userRepository.update(email, updateUserDto);
   }
 }
